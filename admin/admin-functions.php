6425c026c4c415f3c4fdc02122dd9eb05db9225b
León Cantillo <leonjob13@gmail.com>
2024-08-16 18:58:37 -0500

    Admin notice if WooCommerce is not active.

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -3,3 +3,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+
+// Admin notice if WooCommerce is not active.
+function safp_admin_notice_woocommerce_required() {
+    if ( ! class_exists( 'WooCommerce' ) ) {
+        ?>
+        <div class="notice notice-error">
+            <p><?php _e( 'The Search By Advanced Filter plugin requires WooCommerce to be installed and active.', 'search_advanced_filter' ); ?></p>
+        </div>
+        <?php
+    }
+}
+add_action( 'admin_notices', 'safp_admin_notice_woocommerce_required' );
